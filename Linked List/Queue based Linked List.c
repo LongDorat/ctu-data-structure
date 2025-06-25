933c3a0a6c5b959421bd8afe87485c2fbbceeab7
Long Dorat <longdorat@outlook.com>
2025-06-25 15:53:14 +0700

    feat(Queue bases Linked List.c): question 3

--- Linked List/Queue based Linked List.c
+++ Linked List/Queue based Linked List.c
@@ -26,3 +26,13 @@ int is_empty(const Queue *queue)
 {
     return queue->size == 0;
 }
+
+void en_queue(ElementType x, Queue *queue)
+{
+    struct Node *newNode = malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = NULL;
+    queue->rear->next = newNode;
+    queue->rear = newNode;
+    queue->size++;
+}
