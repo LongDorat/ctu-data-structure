7f87967b07a6ee730bb2fd426fd5174da38cb4cc
Long Dorat <longdorat@outlook.com>
2025-07-07 14:40:12 +0700

    feat(Sorting exercises 1.c): question 2

--- Sorting/Sorting exercises 1.c
+++ Sorting/Sorting exercises 1.c
@@ -18,8 +18,8 @@ void shellSort(int *nums, int n)
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
+    int n, k;
+    scanf("%d %d", &n, &k);
 
     int *nums = (int *)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
@@ -29,10 +29,18 @@ int main()
 
     shellSort(nums, n);
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", nums[i]);
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("%d ", nums[i]);
+    // }
+
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < k; i++)
+        sum1 += nums[i];
+    for (int i = n - 1; i >= n - k; i--)
+        sum2 += nums[i];
+
+    printf("%d %d", sum1, sum2);
 
     free(nums);
     return 0;
